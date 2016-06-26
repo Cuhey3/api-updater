a0626468547e28e483307bd422980449062a3cb1
nanashi <nanashi@nanashi-pc>
2016-06-27 01:53:32 +0900

    added route filtering

--- src/main/java/com/heroku/apiupdater/routes/MyRoute.java
+++ src/main/java/com/heroku/apiupdater/routes/MyRoute.java
@@ -1,6 +1,8 @@
 package com.heroku.apiupdater.routes;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Predicate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.rx.ReactiveCamel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,17 +15,26 @@ public class MyRoute extends RouteBuilder {
     @Autowired
     public MyRoute(CamelContext context) {
         ReactiveCamel rx = new ReactiveCamel(context);
-        Observable<String> fooObservable = rx.toObservable("direct:foo", String.class);
-        Observable<String> barObservable = rx.toObservable("direct:bar", String.class);
-        Observable<String> combineLatest = Observable.combineLatest(fooObservable, barObservable, (foo, bar) -> {
-            return foo + bar;
-        });
+        Observable<String> fooObservable
+                = rx.toObservable("direct:foo", String.class);
+
+        Observable<String> barObservable
+                = rx.toObservable("direct:bar", String.class);
+
+        Observable<String> combineLatest
+                = Observable.combineLatest(fooObservable, barObservable,
+                        (foo, bar) -> {
+                            return foo + bar;
+                        });
+        
         rx.sendTo(combineLatest, "direct:combine");
     }
 
     @Override
     public void configure() throws Exception {
-        from("timer:foo?period=5s").setBody(constant("foo")).to("direct:foo");
+        from("timer:foo?period=5s").setBody(constant("foo"))
+                .filter((Exchange exchange) -> Math.random() > 0.7)
+                .to("direct:foo");
         from("timer:bar?period=7s").setBody(constant("bar")).to("direct:bar");
         from("direct:combine").to("log:foobar");
     }
