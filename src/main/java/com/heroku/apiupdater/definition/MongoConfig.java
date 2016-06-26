1ddbd7732ab503892cadda504cae208297e09e40
nanashi <nanashi@nanashi-pc>
2016-06-27 00:24:38 +0900

    updated MongoConfig

--- src/main/java/com/heroku/apiupdater/definition/MongoConfig.java
+++ src/main/java/com/heroku/apiupdater/definition/MongoConfig.java
@@ -13,7 +13,8 @@ public class MongoConfig {
 
     public String ownMongoURI;
     public String ownMongoDatabase;
-    public Document settings;
+    public Document ownSettings;
+    public Document masterSettings;
 
     public MongoConfig() {
         ownMongoURI = System.getenv("MONGODB_URI");
@@ -21,24 +22,42 @@ public class MongoConfig {
             ownMongoURI = System.getenv("API_UPDATER_MONGOLAB_URI");
         }
         if (ownMongoURI != null) {
-            String[] split = ownMongoURI.split("/");
-            ownMongoDatabase = split[split.length - 1];
+            ownMongoDatabase = getDatabaseName(ownMongoURI);
         }
-        try (MongoClient mongoClient
-                = new MongoClient(new MongoClientURI(ownMongoURI))) {
-
-            FindIterable<Document> find
-                    = mongoClient.getDatabase(ownMongoDatabase)
-                    .getCollection("settings").find();
-
-            if (find.iterator().hasNext()) {
-                settings = find.first();
-            }
-        }
+        ownSettings = getSettings(ownMongoURI);
     }
 
     @Bean(name = "api-updater")
-    MongoClient mongoClientApiUpdater() throws UnknownHostException {
+    MongoClient apiUpdaterMongoClient() throws UnknownHostException {
         return new MongoClient(new MongoClientURI(ownMongoURI));
+    }
+
+    @Bean(name = "master")
+    MongoClient masterMongoClient() throws UnknownHostException {
+        String masterMongoUri
+                = ownSettings.get("MASTER_MONGOLAB_URI", String.class);
+
+        masterSettings = getSettings(masterMongoUri);
+        return new MongoClient(new MongoClientURI(masterMongoUri));
+    }
+
+    private String getDatabaseName(String mongoUri) {
+        String[] split = mongoUri.split("/");
+        return split[split.length - 1];
+    }
+
+    public final Document getSettings(String mongoUri) {
+        try (MongoClient mongoClient
+                = new MongoClient(new MongoClientURI(mongoUri))) {
+
+            FindIterable<Document> find
+                    = mongoClient.getDatabase(getDatabaseName(mongoUri))
+                    .getCollection("settings").find();
+
+            if (find.iterator().hasNext()) {
+                return find.first();
+            }
+        }
+        return null;
     }
 }
