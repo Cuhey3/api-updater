56e658ce80181998aaab7ec500f961a8f7e876f4
doi-shuhei <doi@nagisalabs.org>
2016-06-27 11:51:34 +0900

    added MongoConfig snapshot/diff

--- src/main/java/com/heroku/apiupdater/definition/MongoConfig.java
+++ src/main/java/com/heroku/apiupdater/definition/MongoConfig.java
@@ -11,53 +11,66 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MongoConfig {
 
-    public String ownMongoURI;
-    public String ownMongoDatabase;
-    public Document ownSettings;
-    public Document masterSettings;
+  public String ownMongoURI;
+  public String ownMongoDatabase;
+  public Document ownSettings;
+  public Document masterSettings;
 
-    public MongoConfig() {
-        ownMongoURI = System.getenv("MONGODB_URI");
-        if (ownMongoURI == null) {
-            ownMongoURI = System.getenv("API_UPDATER_MONGOLAB_URI");
-        }
-        if (ownMongoURI != null) {
-            ownMongoDatabase = getDatabaseName(ownMongoURI);
-        }
-        ownSettings = getSettings(ownMongoURI);
+  public MongoConfig() {
+    ownMongoURI = System.getenv("MONGODB_URI");
+    if (ownMongoURI == null) {
+      ownMongoURI = System.getenv("API_UPDATER_MONGOLAB_URI");
     }
-
-    @Bean(name = "api-updater")
-    MongoClient apiUpdaterMongoClient() throws UnknownHostException {
-        return new MongoClient(new MongoClientURI(ownMongoURI));
+    System.out.println(ownMongoURI);
+    if (ownMongoURI != null) {
+      ownMongoDatabase = getDatabaseName(ownMongoURI);
     }
+    ownSettings = getSettings(ownMongoURI);
+    masterSettings
+            = getSettings(ownSettings.get("MASTER_MONGOLAB_URI", String.class));
+  }
 
-    @Bean(name = "master")
-    MongoClient masterMongoClient() throws UnknownHostException {
-        String masterMongoUri
-                = ownSettings.get("MASTER_MONGOLAB_URI", String.class);
+  @Bean(name = "master")
+  MongoClient masterMongoClient() throws UnknownHostException {
+    String masterMongoUri
+            = ownSettings.get("MASTER_MONGOLAB_URI", String.class);
 
-        masterSettings = getSettings(masterMongoUri);
-        return new MongoClient(new MongoClientURI(masterMongoUri));
+    return new MongoClient(new MongoClientURI(masterMongoUri));
+  }
+
+  @Bean(name = "snapshot")
+  MongoClient snapshotMongoClient() throws UnknownHostException {
+    String snapshotMongoUri
+            = masterSettings.get("SNAPSHOT_MONGOLAB_URI", String.class);
+
+    return new MongoClient(new MongoClientURI(snapshotMongoUri));
+  }
+
+  @Bean(name = "diff")
+  MongoClient diffMongoClient() throws UnknownHostException {
+    String snapshotMongoUri
+            = masterSettings.get("DIFF_MONGOLAB_URI", String.class);
+
+    return new MongoClient(new MongoClientURI(snapshotMongoUri));
+  }
+
+  private String getDatabaseName(String mongoUri) {
+    String[] split = mongoUri.split("/");
+    return split[split.length - 1];
+  }
+
+  public final Document getSettings(String mongoUri) {
+    try (MongoClient mongoClient
+            = new MongoClient(new MongoClientURI(mongoUri))) {
+
+      FindIterable<Document> find
+              = mongoClient.getDatabase(getDatabaseName(mongoUri))
+              .getCollection("settings").find();
+
+      if (find.iterator().hasNext()) {
+        return find.first();
+      }
     }
-
-    private String getDatabaseName(String mongoUri) {
-        String[] split = mongoUri.split("/");
-        return split[split.length - 1];
-    }
-
-    public final Document getSettings(String mongoUri) {
-        try (MongoClient mongoClient
-                = new MongoClient(new MongoClientURI(mongoUri))) {
-
-            FindIterable<Document> find
-                    = mongoClient.getDatabase(getDatabaseName(mongoUri))
-                    .getCollection("settings").find();
-
-            if (find.iterator().hasNext()) {
-                return find.first();
-            }
-        }
-        return null;
-    }
+    return null;
+  }
 }
--- src/main/java/com/heroku/apiupdater/routes/MyRoute.java
+++ src/main/java/com/heroku/apiupdater/routes/MyRoute.java
@@ -2,7 +2,6 @@ package com.heroku.apiupdater.routes;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
-import org.apache.camel.Predicate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.rx.ReactiveCamel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,31 +11,31 @@ import rx.Observable;
 @Component
 public class MyRoute extends RouteBuilder {
 
-    @Autowired
-    public MyRoute(CamelContext context) {
-        ReactiveCamel rx = new ReactiveCamel(context);
-        Observable<String> fooObservable
-                = rx.toObservable("direct:foo", String.class);
+  @Autowired
+  public MyRoute(CamelContext context) {
+    ReactiveCamel rx = new ReactiveCamel(context);
+    Observable<String> fooObservable
+            = rx.toObservable("direct:foo", String.class);
 
-        Observable<String> barObservable
-                = rx.toObservable("direct:bar", String.class);
+    Observable<String> barObservable
+            = rx.toObservable("direct:bar", String.class);
 
-        Observable<String> combineLatest
-                = Observable.combineLatest(fooObservable, barObservable,
-                        (foo, bar) -> {
-                            return foo + bar;
-                        });
-        
-        rx.sendTo(combineLatest, "direct:combine");
-    }
+    Observable<String> combineLatest
+            = Observable.combineLatest(fooObservable, barObservable,
+                    (foo, bar) -> {
+                      return foo + bar;
+                    });
 
-    @Override
-    public void configure() throws Exception {
-        from("timer:foo?period=5s").setBody(constant("foo"))
-                .filter((Exchange exchange) -> Math.random() > 0.7)
-                .to("direct:foo");
-        from("timer:bar?period=7s").setBody(constant("bar")).to("direct:bar");
-        from("direct:combine").to("log:foobar");
-    }
+    rx.sendTo(combineLatest, "direct:combine");
+  }
+
+  @Override
+  public void configure() throws Exception {
+    from("timer:foo?period=5s").setBody(constant("foo"))
+            .filter((Exchange exchange) -> Math.random() > 0.7)
+            .to("direct:foo");
+    from("timer:bar?period=7s").setBody(constant("bar")).to("direct:bar");
+    from("direct:combine").to("log:foobar");
+  }
 
 }
